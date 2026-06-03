8b943d61d4d6947ded76572ccbed25c18a28072c
awesumdude42 <sammyiyers@gmail.com>
2026-06-03 15:08:09 -0700

    i added poseControls and broke some stuff DYLANNN PLS HALP IM SRY

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { Plus } from "lucide-react";
 import { Switch } from "@/components/ui/switch";
 import { Input } from "@/components/ui/input";
 import { Field, FieldLabel } from "@/components/ui/field";
+import PoseControls from "@/components/poseControls";
 
 
 export default function Home() {
@@ -18,76 +19,13 @@ export default function Home() {
       >
         <ResizablePanel defaultSize="16.7%" maxSize="30%" minSize="7.5%">
       
-          <span className="flex h-full flex-col">
-  
-            <div className="flex justify-center p-4 text-3xl font-bold text-white">
-              Poses
-              
-            </div>
-            
-              <Button className="flex m-4 bg-[#C00000] text-white">
-                  <Plus/>
-                  Add Pose  
-              </Button>
-            <ScrollArea className="w-full flex-1 min-h-0 rounded-md border p-4">
-              <div className="flex text-white">
-                 <Accordion type="single" collapsible defaultValue="item-1">
-                  <AccordionItem value="item-1">
-                    <AccordionTrigger>Pose 1</AccordionTrigger>
-                    <AccordionContent> 
-                
-                      <div className="flex flex-col gap-3 pt-2">
-                        <div className="flex flex-col gap-1">
-                          <FieldLabel htmlFor="x-input" className="text-xs text-muted-foreground">
-                            <p className="text-white">Enter x:</p>
-                          </FieldLabel>
-                          <Input
-                            id="x-input"
-                            type="text"
-                            placeholder="X"
-                            className="w-full" 
-                          />
-                        </div>
-
-                        <div className="flex flex-col gap-1.5">
-                          <FieldLabel htmlFor="y-input" className="text-xs text-muted-foreground">
-                            <p className="text-white">Enter y:</p>
-                          </FieldLabel>
-                          <Input
-                            id="y-input"
-                            type="text"
-                            placeholder="Y"
-                            className="w-full" 
-                          />
-                        </div>
-                        
-                       
-                        <div className="flex items-center space-x-2">
-                          <Switch id="arc-pose"  />
-                          <label htmlFor="arc-pose" className="text-sm select-none cursor-pointer">
-                            Arc Pose
-                          </label>
-                        </div>
-
-                        <div className="flex items-center space-x-2">
-                          <label htmlFor="local" className="text-sm select-none cursor-pointer">
-                            Local
-                          </label>
-                          <Switch id="local-global"/>
-                          <label htmlFor="global" className="text-sm select-none cursor-pointer">
-                            Global
-                          </label>
-                        </div>
-                      </div>
-                     </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-              </div>
-            </ScrollArea>
-
+          <span>
+            <PoseControls/>
           </span>
         </ResizablePanel>
         <ResizableHandle withHandle />
+
+        
         <ResizablePanel defaultSize="58.3%" minSize="40%">
           <div className="flex h-full items-center justify-center">
               <span className="font-semibold">
