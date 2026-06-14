5fd99eebd460ce5e5cc98de1ae9ef610a2cd743e
awesumdude42 <sammyiyers@gmail.com>
2026-06-14 15:53:53 -0700

    path add and remove

--- src/components/path-controls.tsx
+++ src/components/path-controls.tsx
@@ -11,7 +11,7 @@ import {
     UncontrolledTreeEnvironment, 
     TreeItem 
 } from "react-complex-tree";
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { ChevronDown, ChevronDownCircle, CircleMinus, CirclePlus, GripVertical, Plus} from "lucide-react";
 import { Input } from "./ui/input";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./ui/accordion";
@@ -32,33 +32,57 @@ interface pathControlProps {
 
 export default function PathControls({ Poses }: pathControlProps) {
     const poses = Poses || [];
-    const [tricks, setTricks] = useState([
-    { id: "1", title: "The 900"},
-    { id: "2", title: "Indy Backflip"},
-    { id: "3", title: "Pizza Guy"},
-    {
-      id: "4",
-      title: "360 Varial McTwist",
-    },
-  ]);
-    
+    const [paths, setPaths] = useState<Path[]>([]);
+    const addPath = () => {
+        setPaths((prevPaths) => {
+            const nextNumber = prevPaths.length + 1;
+            
+        
+            const firstPath = prevPaths.length === 0;
+            const lastPath = !firstPath ? prevPaths[prevPaths.length - 1] : null;
+            const prevEndPose = lastPath ? (lastPath.poses.at(-1) || null) : null;
+            const newPath: Path = {
+                id: `path-${Date.now()}`, 
+                name: `Path ${nextNumber}`,
+                poses: [], 
+                callbacks: [{
+                    methodName: "",
+                    distance: "",
+                    distValue: false,
+                }],
+                firstPath: firstPath,
+                prevEndPose: prevEndPose
+            };
 
+            return [...prevPaths, newPath]; 
+        });
+    };
+    const updatePath = (id: string, updatedFields: Partial<Path>) => {
+        setPaths((prev) =>
+            prev.map((path) => 
+            path.id === id ? { ...path, ...updatedFields } : path
+            )
+        );
+    };
 
-
-
+    const deletePath = useCallback((id: string) => {
+            setPaths((prev) => prev.filter((path) => path.id !== id));
+    }, []);
     return (
         <div className="flex h-full flex-col">
             <div className="flex justify-center p-4 text-3xl font-bold text-white">
                 Paths
             </div>
-            <Button className="flex m-4">
+            <Button className="flex m-4" onClick={addPath}>
                 <Plus/>
                 Add Path
             </Button>
            
             <ScrollArea className="w-full flex-1  min-h-0 border-t p-4">
+
                 <div className="flex text-white flex-col">
-                    <Accordion type="single" collapsible defaultValue="item-1" className="w-full" >
+                    {paths.map((path) => (
+                    <Accordion type="single" key={path.id} collapsible defaultValue="item-1" className="w-full" >
                         <AccordionItem value="item-1">
                         <div className="flex flex-row w-full">
                             <AccordionTrigger>
@@ -67,8 +91,9 @@ export default function PathControls({ Poses }: pathControlProps) {
                                         id={"PATH"}
                                         type="text"
                                         placeholder="Pose Name"
-                                        defaultValue={"Path1"}
+                                        value={path.name}
                                         className="transition-colors mr-2 focus-visible:border-red-500 focus-visible:ring-red-500"
+                                        onChange={(e)=>updatePath(path.id,{name:e.target.value})}
                                         onClick={(e) => e.stopPropagation()}
                                         onKeyDown={(e) => {
                                             if (e.key === " ") {
@@ -80,7 +105,7 @@ export default function PathControls({ Poses }: pathControlProps) {
                                 </div>
                             </AccordionTrigger>
                             
-                            <Button className=" ml-2 mt-2 bg-[#11111] hover:bg-[#11111]" >
+                            <Button className=" ml-2 mt-2 bg-[#11111] hover:bg-[#11111]" onClick={()=>deletePath(path.id)}>
                                 <CircleMinus color="#C00000"/>
                             </Button>
                             
@@ -190,6 +215,7 @@ export default function PathControls({ Poses }: pathControlProps) {
                         </AccordionContent>
                         </AccordionItem>
                     </Accordion>
+                    ))}
                 </div>
             </ScrollArea>
         </div>
